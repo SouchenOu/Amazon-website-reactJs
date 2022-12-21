717969c4f9bfcb8c54b5061e7919f9b80c07aa02
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2022-12-21 10:59:09 +0100

    start amazon website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello souchen</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcJImHbPmZJs1avbHY5RoE2eDv5tbLiZA",
+    authDomain: "web-99ebd.firebaseapp.com",
+    projectId: "web-99ebd",
+    storageBucket: "web-99ebd.appspot.com",
+    messagingSenderId: "375307200184",
+    appId: "1:375307200184:web:737a1ffeb9aeb67b2f364d",
+    measurementId: "G-V98WXWT56M"
+  };
