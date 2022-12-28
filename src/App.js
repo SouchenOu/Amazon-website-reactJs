8e8fb82b4365bab772554f6302f11b19dc0aaf31
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2022-12-28 11:08:56 +0100

    start checkout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 import './App.css';
 import Header from './header';
-import Home from './home'
+import Home from './home';
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
+
 function App() {
   return (
+    <Router>
     <div className="app">
+      <Switch>
+          <Route path="/checkout">
       <Header />
-      <Home/>
+        <h1>I am a checkout , Smach the like button</h1>
+      </Route>
+      <Route path='/'>
+      <Header />
+        <Home/>
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
