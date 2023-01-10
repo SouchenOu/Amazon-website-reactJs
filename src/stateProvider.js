4357d687cb94f658012ef9fd8cc99e61b87f99da
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-10 11:46:40 +0100

    continue

--- src/stateProvider.js
+++ src/stateProvider.js
@@ -1,11 +1,14 @@
 import React, { createContext, useContext, useReducer} from "react";
 
+//prepare the data layer
 export const stateContext = createContext();
 
+//wrap our app and provide the data layer
 export const stateProvider = ({reducer, initialState, children} ) => (
 
     <stateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </stateContext.Provider>
 );
+//Pull information from the data layer
 export const useStateValue = () => useContext(stateContext);
